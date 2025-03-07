d5ebd5bbd0ba9acace859e9d1508a01b338dd622
HamzahM03 <138250911+HamzahM03@users.noreply.github.com>
2025-03-06 23:44:17 -0500

    connected mongodb to server

--- server/config/db.js
+++ server/config/db.js
@@ -5,13 +5,10 @@ dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (error) {
-        console.error('MongoDB Connection Error: ', error);
+        console.error('MongoDB Connection Error:', error);
         process.exit(1); // Exit process with failure
     }
 };
