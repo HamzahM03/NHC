c249d1efdf30b3fcbf87854d01a5a1f6914c7a16
HamzahM03 <138250911+HamzahM03@users.noreply.github.com>
2025-03-07 01:31:12 -0500

    Setup frontend and display total kids on dashboard

--- server/server.js
+++ server/server.js
@@ -3,9 +3,18 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import kidRoutes from "./routes/kidRoutes.js"; // Import routes
 
+import cors from "cors";
+
 dotenv.config(); // Load environment variables
 
 const app = express();
+
+// Enable CORS
+app.use(cors({
+    origin: "http://localhost:5174", // Allow frontend
+    credentials: true
+}));
+
 
 // Connect to MongoDB
 connectDB();
