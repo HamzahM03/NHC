0fa4ea5199ca8a27358cde32b5666113691d1bb4
HamzahM03 <138250911+HamzahM03@users.noreply.github.com>
2025-04-30 09:08:48 -0400

    connect to mongodb, create participantSchema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express"
+import connectDB from "./config/db.js"
+
+
 
 const app = express()
+
+connectDB();
 
 
 app.get("/", (req,res)=>{
